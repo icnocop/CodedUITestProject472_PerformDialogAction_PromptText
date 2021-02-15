ea3a16438c61130e36b48cc80800451740f47057
Rami Abughazaleh <icnocop@users.noreply.github.com>
2021-02-14 18:43:43 -0800

    Initial commit

---
+++ CodedUITestProject472/MyTestClass.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodedUITestProject472
+{
+    [TestClass]
+    public class MyTestClass
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (!Playback.IsInitialized)
+            {
+                Playback.Initialize();
+            }
+        }
+
+        [TestMethod]
+        public void InputTextInBrowserPrompt()
+        {
+            BrowserWindow.CurrentBrowser = "IE";
+
+            var window = BrowserWindow.Launch();
+
+            window.ExecuteScript("prompt('Please enter your name', 'default input text');");
+
+            window.PerformDialogAction(BrowserDialogAction.PromptText, "your name");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (Playback.IsInitialized)
+            {
+                Playback.Cleanup();
+            }
+        }
+    }
+}
---
+++ CodedUITestProject472/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodedUITestProject472")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodedUITestProject472")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66323eea-8155-4116-97d8-511df84c4cf2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
